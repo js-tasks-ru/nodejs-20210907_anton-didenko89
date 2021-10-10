7ee66377a2db591c45c468f39a90a9137ee56306
Anton Didenko <anton.didenko89@gmail.com>
2021-10-10 16:33:10 +0300

    3rd task from 6 module is solved

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,19 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  let products = await Product.find({$text: {$search: ctx.request.query.query}},
+    {score: {$meta: "textScore"}}
+  ).sort( { score: { $meta: "textScore" } } );
+
+  ctx.body = {products: products.map((p) => {
+      return {
+        id: p._id,
+        title: p.title,
+        images: p.images,
+        category: p.category,
+        subcategory: p.subcategory,
+        price: p.price,
+        description: p.description
+      }
+    })};
 };
