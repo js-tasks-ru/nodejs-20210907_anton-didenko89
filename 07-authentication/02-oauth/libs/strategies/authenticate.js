f1934e3b053ab0bb76d197de1cff32f78a8bb169
Anton Didenko <anton.didenko89@gmail.com>
2021-10-16 17:23:05 +0300

    removed console.log()

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,15 +1,13 @@
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-  console.log(email, displayName);
-
   if (!email) return done(null, false, `Не указан email`);
 
   await User.findOne({ email: email }, async function (err, user) {
     if (err) return done(err);
 
     if (!user) {
-      user = await User.create({email: email, displayName: displayName}, function(error, user) {
+      user = await User.create({email: email, displayName: displayName}, (error, user) => {
         if(error) return done(error);
 
         return done(null, user);
